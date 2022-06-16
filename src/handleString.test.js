50a0c5efdc750741c1c41219b9b3de8c6b2fdeb6
Diorgenes Morais <diorgenesmorais@gmail.com>
2022-06-16 19:48:41 -0300

    feat: created first test

---
+++ src/handleString.test.js
@@ -0,0 +1,7 @@
+import { formatInDatePattern } from './handleString'
+
+describe('Converting texts', () => {
+    it('formatInDatePattern() should be a function', () => {
+        expect(formatInDatePattern).toBeInstanceOf(Function)
+    })
+})
