59070a7a35c8bed3118e968bfef1168fa1737d39
Diorgenes Morais <diorgenesmorais@gmail.com>
2022-06-16 23:50:12 -0300

    feat: added the other tests

--- src/handleString.js
+++ src/handleString.js
@@ -1,1 +1,17 @@
-export const formatInDatePattern = () => {}
+export const errorMessage = 'The parameter must be a string'
+
+/**
+ * Format text to date pattern
+ * 
+ * @param {string} text 
+ * @returns date at the pattern dd/MM/yyyy
+ */
+export const formatInDatePattern = text => {
+    if (typeof text != 'string') {
+        throw new TypeError(errorMessage)
+    }
+
+    return text
+            .replace(/\D/g, '') // remove no-digits
+            .replace(/(\d{4})(\d{2})(\d{2})/, '$3/$2/$1') // format dd/MM/yyyy
+}
--- src/handleString.test.js
+++ src/handleString.test.js
@@ -1,7 +1,43 @@
-import { formatInDatePattern } from './handleString'
+import { errorMessage, formatInDatePattern } from './handleString'
 
 describe('Converting texts', () => {
     it('formatInDatePattern() should be a function', () => {
         expect(formatInDatePattern).toBeInstanceOf(Function)
+    })
+
+    it('formatInDatePattern() should return "16/06/2022"', () => {
+        const actual = formatInDatePattern('2022-06-16')
+        const expected = '16/06/2022'
+
+        expect(actual).toBe(expected)
+    })
+
+    it('formatInDatePattern() should return "13/02/2022"', () => {
+        const actual = formatInDatePattern('20220213')
+        const expected = '13/02/2022'
+
+        expect(actual).toBe(expected)
+    })
+
+    it('formatInDatePattern() should throw a TypeError', () => {
+        try {
+            formatInDatePattern(null)
+        } catch (error) {
+            expect(error.name).toBe('TypeError')
+        }
+    })
+
+    it(`formatInDatePattern() should throw a TypeError with message "${errorMessage}"`, () => {
+        try {
+            formatInDatePattern(null)
+        } catch (error) {
+            expect(error.message).toBe(errorMessage)
+        }
+    })
+
+    it(`formatInDatePattern() should return empty`, () => {
+        const actual = formatInDatePattern('diorgenes')
+
+        expect(actual).toBe('')
     })
 })
