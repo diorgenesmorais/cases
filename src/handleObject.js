bedbc62310884070277e5c6e7779f691f3beb8c3
Diorgenes Morais <diorgenesmorais@gmail.com>
2022-06-18 19:31:39 -0300

    feat: remove object properties

--- src/handleObject.js
+++ src/handleObject.js
@@ -6,3 +6,15 @@
  * @returns object without the given property
  */
 export const removeObjectProperty = (propertyName, obj) => (delete obj[propertyName])
+
+/**
+ * Remove object properties
+ * 
+ * @param {array} properties to remove
+ * @param {Object} obj 
+ * @returns object without properties
+ */
+export const removeObjectProperties = (properties, obj) => {
+    properties.forEach(key => delete obj[key]);
+    return obj
+}
--- src/handleObject.test.js
+++ src/handleObject.test.js
@@ -1,4 +1,4 @@
-import { removeObjectProperty } from "./handleObject"
+import { removeObjectProperties, removeObjectProperty } from "./handleObject"
 
 describe('Using computed property name', () => {
     it('removeObjectProperty() should be a function', () => {
@@ -16,5 +16,21 @@ describe('Using computed property name', () => {
         const actual = removeObjectProperty('CLASSE', resource)
 
         expect(actual.CLASSE).toBeUndefined()
+    })
+
+    it('Should removed the properties of list', () => {
+        const resource = {
+            CLASSE: '140',
+            ATTS: 'Alteração Tarifa Social',
+            RCTS: 'Recadastramento Tarifa Social',
+            RVTS: 'Revisão Cadastral Tarifa Social'
+        }
+
+        const actual = removeObjectProperties(['RVTS', 'ATTS', 'RCTS'], resource)
+        const expected = JSON.stringify({
+            CLASSE: '140'
+        })
+
+        expect(JSON.stringify(actual)).toBe(expected)
     })
 })
